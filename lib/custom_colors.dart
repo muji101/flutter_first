5710d251fdc60737f2173f61a580b2f4f91f75d2
muji101 <mujikuwat91@gmail.com>
2022-02-15 22:19:01 +0700

    first

---
+++ lib/custom_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// kode hex color
+
+Color darkBlue = Color(0XFF2c2c54);
+Color darkSilver = Color(0XFF84817a);
---
+++ lib/font_style.dart
@@ -0,0 +1,16 @@
+import 'package:cashy_apps/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+TextStyle mainHeader = TextStyle(
+    fontSize: 26,
+    color: darkBlue,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700 
+);
+
+TextStyle subHeader = TextStyle(
+    fontSize: 16,
+    color: darkSilver,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w300 
+);
